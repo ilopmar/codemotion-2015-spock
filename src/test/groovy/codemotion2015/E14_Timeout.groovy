c42192a8c72831d14ed8b138dad602904e780e31
Iván López <lopez.ivan@gmail.com>
2015-11-23 22:46:19 +0100

    Example 14: @Timeout

---
+++ src/test/groovy/codemotion2015/E14_Timeout.groovy
@@ -0,0 +1,20 @@
+package codemotion2015
+
+import spock.lang.Ignore
+import spock.lang.Specification
+import spock.lang.Timeout
+
+import java.util.concurrent.TimeUnit
+
+@Ignore("Remove to check that the test fails")
+class E14_Timeout extends Specification {
+
+    @Timeout(value = 500, unit = TimeUnit.MILLISECONDS)
+    void 'should fail after 500 milliseconds'() {
+        given:
+            Thread.sleep 500
+
+        expect:
+            true
+    }
+}
